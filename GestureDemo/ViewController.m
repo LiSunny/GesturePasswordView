81f54ddc136c03e16c41823e76edefda2b0dbfcd
Ccli <ccli321@163.com>
2016-07-29 15:29:12 +0800

    Initial Commit

---
+++ GestureDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GestureDemo
+//
+//  Created by HeavenLi on 16/7/29.
+//  Copyright © 2016年 HeavenLi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ GestureDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GestureDemo
+//
+//  Created by HeavenLi on 16/7/29.
+//  Copyright © 2016年 HeavenLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
